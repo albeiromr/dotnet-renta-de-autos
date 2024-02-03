837288c74811e4dba73dd0f8c4c34164eb337c56
Albeiro Marín <xlandru@hotmail.com>
2024-02-03 16:58:49 -0500

    Agrega parte de las configuraciones de la base de datos

---
+++ Infrastructure/Configurations/RentalConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Rentals;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal sealed class RentalConfiguration : IEntityTypeConfiguration<Rental>
+{
+    public void Configure(EntityTypeBuilder<Rental> builder)
+    {
+        builder.ToTable("rentals");
+        builder.HasKey(rental => rental.Id);
+    }
+}
---
+++ Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Users;
+using Domain.Users.ObjectValues;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+        builder.HasKey(user => user.Id);
+
+        builder.Property(user => user.Name)
+            .HasMaxLength(200)
+            .HasConversion(name => name!.name, value => new Name(value));
+
+        builder.Property(user => user.LastName)
+            .HasMaxLength(200)
+            .HasConversion(lastName => lastName!.lastName, value => new LastName(value));
+
+        builder.Property(user => user.Email)
+            .HasMaxLength(200)
+            .HasConversion(email => email!.email, value => new Email(value));
+        
+        //Haciendo que el email sea único
+        builder.HasIndex(user => user.Email).IsUnique();
+
+    }
+}
