0c91340d93817cfa1ee4d1efdb5e449e47528760
Albeiro Marín <xlandru@hotmail.com>
2024-02-05 09:45:34 -0500

    ajusta sql de los queries

--- Application/Rentals/Responses/RentalResponse.cs
+++ Application/Rentals/Responses/RentalResponse.cs
@@ -6,14 +6,7 @@ public sealed class RentalResponse
     public Guid UserId { get; init; }
     public Guid VehicleId { get; init; }
     public int Status { get; init; }
-    public decimal RentalPeriodPrice { get; init; }
-    public string? RentalPeriodPriceCurrency { get; init; }
-    public decimal MaintainancePrice { get; init; }
-    public string? MaintainancePriceCurrency { get; init; }
-    public decimal PremiumAccessoriesPrice { get; init; }
-    public string? PremiumAccessoriesPriceCurrency { get; init; }
-    public decimal TotalPrice { get; init; }
-    public decimal TotalPriceCurrency { get; init; }
+    public decimal Price { get; init; }
     public DateOnly DurationInit { get; init; }
     public DateOnly DurationEnd { get; init; }
     public DateOnly CreationDate { get; init; }
